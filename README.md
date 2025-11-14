c45a4612b7ebaa018e1ff0e023cc3cd09a228822
Andrei V. Lepikhov <lepihov@gmail.com>
2025-11-14 09:36:36 +0100

    Step ahead

--- README.md
+++ README.md
@@ -10,4 +10,7 @@ psql -f schema-sales.sql
 Simple launch:
 ```
 pgbench -n -t 1 -f sale.pgb
+
+psql -f ../../testdb/schema-sales.sql
+pgbench -n -c 5 -j 5 -f ../../testdb/sale.pgb -T 30 -P 3 --max-tries=0
 ```
--- schema-sales.sql
+++ schema-sales.sql
@@ -1,6 +1,7 @@
 DROP TABLE IF EXISTS
   supplies,sales,exceptions,depots,products,periods
 CASCADE;
+DROP FUNCTION IF EXISTS do_sale;
 
 \set depots_num 10
 \set products_num 1000
@@ -51,6 +52,42 @@ INSERT INTO products (product_id,label)
 -- Initially, each depot contains zero value of each product: first incoming
 -- transaction will perform supply.
 INSERT INTO supplies (depot_id,product_id,quantity,quantity_predicted)
-  SELECT depot_id,product_id,0,1000 FROM depots, products;
+  SELECT depot_id,product_id,0,100 FROM depots, products;
 
 ANALYZE;
+
+/*
+ * Perform sale logic:
+ * 1. Check availability of the product.
+ * 2. Sale it or complain into exceptions.
+ * 3. Write the fact of the sale attempt.
+ *
+ * TODO: In case of unsuccessful sale checl the counter. If it is too big, try
+ * to request products from another depot.
+ */
+CREATE FUNCTION do_sale(d_id integer,pr_id integer, per_id bigint)
+RETURNS VOID AS $$
+DECLARE
+	qty integer;
+BEGIN
+  SELECT quantity AS qty FROM supplies
+    WHERE depot_id = d_id AND product_id = pr_id INTO qty;
+
+  IF (qty > 0) THEN
+    -- Main track
+    UPDATE supplies SET quantity = quantity - 1
+    WHERE depot_id = d_id AND product_id = pr_id;
+  ELSE
+    -- We apologise to the client and mark we need more
+	INSERT INTO exceptions AS e (depot_id,product_id,period,counter)
+      VALUES (d_id, pr_id, per_id, 1)
+      ON CONFLICT (depot_id,product_id,period)
+      DO UPDATE SET counter = e.counter + 1;
+  END IF;
+
+  -- log the sale
+  INSERT INTO sales (depot_id,product_id,period,success)
+    VALUES (d_id, pr_id, per_id, (qty > 0)::boolean);
+END;
+$$ LANGUAGE plpgsql;
+
